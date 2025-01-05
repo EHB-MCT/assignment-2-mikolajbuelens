0f759f73e908f1cacf10fec0db72cfc65fffc2d5
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2025-01-05 13:03:33 +0100

    feat: add text generator with npm package txtgen

--- type-speed-analyser/src/app/components/TypingTest.tsx
+++ type-speed-analyser/src/app/components/TypingTest.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import Button from "./ui/Button";
+import TextGenerator from "./logic/TextGenerator";
+import TypingValidator from "./logic/TypingValidator";
+import { paragraph } from "txtgen";
 
 export default function TypingTest() {
+  const [text, setText] = useState(paragraph());
+
+  useEffect(() => {
+    setText(paragraph());
+  }, []);
+
   return (
     <div className="container h-screen mx-auto p-4 flex justify-center items-center">
       <div className=" w-full p-4 text-white rounded">
@@ -12,27 +22,23 @@ export default function TypingTest() {
 
         <div className="bg-blue-900 w-full p-4 text-white rounded font-mono text-lg mt-4">
           {/* Placeholder text, should be replaced with a random paragraph generator (api/npm/json) */}
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel id
-            tempora saepe repudiandae dolore sequi voluptates, sit repellendus
-            eius nihil quas ratione dolorum a architecto mollitia. Cum quia
-            voluptas quod!
-          </p>
+          <TextGenerator text={text} />
         </div>
         <div className="flex justify-between mt-4">
           <div className="bg-white w-3/4  p-4 rounded">
             {/* Textarea for user input, this will be used to measure type speed */}
-            <textarea
-              className="w-full h-full focus:outline-none text-black"
-              placeholder="Start typing here..."
-            />
+            <TypingValidator text={text} />
           </div>
           <div className="grid grid-rows-4  grid-flow-col w-1/4 gap-2">
             <p className="bg-blue-700  text-white  flex justify-center items-center row-span-4">
               0 WPM
             </p>
 
-            <Button className="row-span-2" text="Reset" onClick={() => console.log("Reset")} />
+            <Button
+              className="row-span-2"
+              text="Reset"
+              onClick={() => console.log("Reset")}
+            />
             <p className="bg-blue-700  text-white flex justify-center items-center row-span-2 ">
               00:00
             </p>
---
+++ type-speed-analyser/src/app/components/logic/TextGenerator.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+// could be used for addition modifications to the text (e.g. translation)
+export default function TextGenerator({ text }) {
+  return (
+    <div>
+      <p>{text}</p>
+    </div>
+  );
+}
