c0b1d82f756c6b208e3763da76f2404c61eb4492
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2025-01-05 13:16:08 +0100

    feat: validate user input of given text, ignore input if incorrect

---
+++ type-speed-analyser/src/app/components/logic/TypingValidator.jsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+
+export default function TypingValidator({ text }) {
+  const [userInput, setUserInput] = useState("");
+
+  const handleInputChange = (e) => {
+    const input = e.target.value;
+    const currentIndex = userInput.length; // current letter index of user
+
+    console.warn(currentIndex);
+    console.warn(userInput[currentIndex]);
+    console.warn(text[currentIndex]);
+
+    //   validating the next lettet
+    if (input[currentIndex] === text[currentIndex]) {
+      setUserInput(input); // if the next letter is incorrect, the input will not be updated
+    }
+  };
+
+  return (
+    <>
+      <textarea
+        className="w-full h-full focus:outline-none text-black"
+        placeholder="Start typing here..."
+        value={userInput}
+        onChange={handleInputChange}
+      />
+      {/* {console.log(text)}
+      {console.log(userInput)} */}
+    </>
+  );
+}
